51cfe32e6fffca24acf6967a353afc19a3553943
Pedro Silva <pedrosilvavpsn@gmail.com>
2021-03-03 22:16:51 +0000

    new commit

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const compression = require('compression');
+const favicon = require('express-favicon');
 const path = require('path');
 const app = express();
 
 app.use(compression());
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(req, res) {
